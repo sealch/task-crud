1194be1196a2b6df4e7a4fdede710df7a790da33
sealch <thesermanarm@gmail.com>
2020-03-27 23:24:18 +0200

    array CRUD

--- index.js
+++ index.js
@@ -1,26 +1,76 @@
-const {ApolloServer, gql} = require('apollo-server-express');
+const { ApolloServer, gql } = require('apollo-server-express');
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 const port = process.env.PORT || 5000;
 
+const tasks = require('./tasks');
+
+// const Cat = mongoose.model('Cat', { name: String });
+//
+// const kitty = new Cat({ name: 'Zildjian' });
+// kitty.save().then(() => console.log('meow'));
+
 const typeDefs = gql`
-    type Query {  
-        hello: String!
+    type Task {
+        id: ID!
+        name: String!
+        done: Boolean!
+    }
+    
+    type Query {
+        allTasks: [Task!]
+    }
+    
+    type Mutation {
+        createTask(name: String!, done: Boolean!): Task! 
+        removeTask(id: ID!): Task
+        updateTask(id: ID!, done: Boolean!): Task!
     }
 `;
 
 const resolvers = {
     Query: {
-        hello: () => 'hello'
+        allTasks: () => tasks
+    },
+    Mutation: {
+        createTask: (_, { name, done }) => {
+            const task = {
+                id: Math.round(Math.random() * 100),
+                name,
+                done
+            };
+            tasks.push(task);
+            return task;
+        },
+        removeTask: (_, { id }) => {
+            const taskIndex = tasks.findIndex(task => task.id === id);
+            const task = tasks[taskIndex];
+            tasks.splice(taskIndex, 1);
+            return task;
+        },
+        updateTask: (_, { id, done }) => {
+            const task = tasks.find(task => task.id === parseInt(id));
+            task.done = done;
+            return task;
+        }
     }
 };
 
-const server = new ApolloServer({
-    typeDefs,
-    resolvers
-});
 
-server.applyMiddleware({ app });
+const startServer = async () => {
 
-app.listen(port, () => console.log(`running on port: ${port}`));
+    const server = new ApolloServer({
+        typeDefs,
+        resolvers
+    });
+
+    server.applyMiddleware({ app });
+
+    // await mongoose.connect('mongodb://localhost:27017/test', {useNewUrlParser: true, useUnifiedTopology: true});
+
+    app.listen(port, () => console.log(`running on port: ${port}`));
+};
+
+startServer();
---
+++ tasks.js
@@ -0,0 +1,9 @@
+module.exports = [{
+    id: 1,
+    name: 'test',
+    done: false
+}, {
+    id: 2,
+    name: 'test2',
+    done: true
+}];
